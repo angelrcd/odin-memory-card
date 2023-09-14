be08ea4abc2ce35b63583e2b7220411300172104
Angel Romero <mrangelrcd@gmail.com>
2023-09-14 16:34:03 +0200

    Prefetch all images on first load

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,11 @@
 import { useEffect, useState } from "react";
 
-export default function Card({ pokemon, handleSelectPokemon }) {
-    const [image, setImage] = useState("/");
-
-    useEffect(() => {
-        fetch(pokemon.url)
-            .then((result) => result.json())
-            .then((json) => setImage(json.sprites.front_default));
-    });
-
+export default function Card({ pokemon, handleSelectPokemon, image }) {
     return (
         <button
             className="rounded-lg bg-slate-400 p-4"
             onClick={() => handleSelectPokemon(pokemon.name)}>
-            <img className="mx-auto" src={image} alt={pokemon.name} />
+            <img className="mx-auto" src={image?.src} alt={pokemon.name} />
             <span>{pokemon.name}</span>
         </button>
     );
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Card from "./Card";
 
 export default function Game({
@@ -6,9 +7,31 @@ export default function Game({
     setBestScore,
     handleSelectPokemon,
 }) {
+    const [images, setImages] = useState(null);
+    const [imagesLoaded, setImagesLoaded] = useState(false);
     let pokemonList;
 
-    if (pokemonFetchResult.length === 0) {
+    useEffect(() => {
+        const imagesObject = {};
+
+        Promise.all(
+            pokemonFetchResult.map((pokemonFetch) =>
+                fetch(pokemonFetch.url).then((result) => result.json())
+            )
+        )
+            .then((res) => {
+                res.forEach((response) => {
+                    const imageElement = new Image();
+                    imageElement.src = response.sprites.front_default;
+                    imagesObject[response.name] = imageElement;
+                });
+                setImages(imagesObject);
+                setImagesLoaded(true);
+            })
+            .catch((err) => console.error(err));
+    }, [pokemonFetchResult]);
+
+    if (pokemonFetchResult.length === 0 || !imagesLoaded) {
         return <p>Loading...</p>;
     } else {
         pokemonList = getRandomElements(pokemonFetchResult, 12);
@@ -21,6 +44,7 @@ export default function Game({
                     key={pokemon.name}
                     pokemon={pokemon}
                     handleSelectPokemon={handleSelectPokemon}
+                    image={images[pokemon.name]}
                 />
             ))}
         </main>
