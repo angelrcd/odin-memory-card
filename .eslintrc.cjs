e526ae65b026e42432bc1cc044b31e1872a41e6f
Angel Romero <mrangelrcd@gmail.com>
2023-09-23 17:58:19 +0200

    Project typescript port first commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,17 +3,21 @@ module.exports = {
     env: { browser: true, es2020: true },
     extends: [
         "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:react/jsx-runtime",
+        "plugin:@typescript-eslint/strict-type-checked",
+        "plugin:@typescript-eslint/stylistic-type-checked",
         "plugin:react-hooks/recommended",
         "prettier",
     ],
-    ignorePatterns: ["dist", ".eslintrc.cjs"],
-    parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-    settings: { react: { version: "18.2" } },
+    ignorePatterns: ["dist", ".eslintrc.cjs", "tailwind.config.js"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: ["./tsconfig.json", "./tsconfig.node.json"],
+        tsconfigRootDir: __dirname,
+    },
     plugins: ["react-refresh"],
     rules: {
-        "react/prop-types": "off",
         "react-refresh/only-export-components": [
             "warn",
             { allowConstantExport: true },
--- src/components/Score.jsx
+++
@@ -1,8 +0,0 @@
-export default function Score({ score, bestScore }) {
-    return (
-        <div className="flex flex-col">
-            <span>Score: {score}</span>
-            <span>Best score: {bestScore}</span>
-        </div>
-    );
-}
